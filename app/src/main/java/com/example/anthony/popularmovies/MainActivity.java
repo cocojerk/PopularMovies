155389867769408045ff49e79e8a0f8cd0ec36f7
cocojerk <sponge751@gmail.com>
2017-03-28 13:22:38 -0400

    Update MainActivity.java

--- app/src/main/java/com/example/anthony/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/anthony/popularmovies/MainActivity.java
@@ -227,18 +227,5 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    public class movieData{
-
-    }
 
 }
